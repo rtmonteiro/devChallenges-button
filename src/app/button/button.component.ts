6e6d1cad577243423782d0dc617754a70ff3f9f1
Ryan Monteiro <monteiroryan10@gmail.com>
2020-10-04 17:48:58 -0300

    feat: adds material icon to dev-button

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'dev-button',
-  template: `<button [ngClass]='classes'>{{text}}</button>`,
+  template: `
+    <button [ngClass]='classes'>
+      <span *ngIf="left_icon_name" class="material-icons" style="margin-right: 10px">{{left_icon_name}}</span>
+      {{text}}
+      <span *ngIf="right_icon_name" class="material-icons" style="margin-left: 10px">{{right_icon_name}}</span>
+    </button>`,
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
 
   @Input() disableShadow = undefined;
   @Input() disabled = undefined;
+  @Input() left_icon_name: string = undefined;
+  @Input() right_icon_name: string = undefined;
   @Input() variant = 'default';
   @Input() color = 'default';
   @Input() size = 'md';
